ab25003e52df025ac62398653aa54cec50cd2c69
oxey <lucoerlemans37@gmail.com>
2025-09-29 12:48:26 +0200

    add functional player to display

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,22 @@ use game::*;
 
 fn main() {
     let p = Player {
-        name: "WHAT".to_string(),
+        name: "your mom".into(),
+        cash: 5,
+        assets: vec![Asset {
+            name: "bank thing".into(),
+            gold_value: 2,
+            silver_value: 3,
+            color: Color::Red,
+            asset_powerup: None,
+        }],
+        liabilities: vec![],
+        hand: vec![],
+        gold: 2,
+        silver: 3,
+        cards_to_grab: 3,
+        assets_to_play: 1,
+        liabilities_to_play: 1,
     };
 
     println!("{}", serde_json::to_string(&p).unwrap());

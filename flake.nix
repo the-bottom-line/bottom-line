55db11acbde1b28c4ad97caf2bc7d5025f90807e
oxey <lucoerlemans37@gmail.com>
2025-09-29 11:01:34 +0200

    create bottom line backend repo

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A devshell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            rust
+            pkg-config
+            # rust-bin.stable.latest.default
+            # (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+          ];
+        };
+      }
+    );
+}

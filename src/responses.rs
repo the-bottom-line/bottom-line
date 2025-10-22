0625aa93951072cf0987d457af5fdf63fe5e47a1
oxey <lucoerlemans37@gmail.com>
2025-10-22 21:59:55 +0200

    remove extra response data types

--- src/responses.rs
+++ src/responses.rs
@@ -5,30 +5,6 @@ use either::Either;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
-#[derive(Debug, Clone, Serialize)]
-pub struct StartGame {
-    cash: u8,
-    #[serde(with = "serde_asset_liability::vec")]
-    hand: Vec<Either<Asset, Liability>>,
-    pickable_characters: Option<PickableCharacters>,
-    player_info: Vec<PlayerInfo>,
-    turn_order: Vec<PlayerId>,
-}
-
-#[derive(Debug, Clone, Serialize)]
-pub struct PlayersInLobby {
-    players: Vec<String>,
-}
-
-#[derive(Debug, Clone, Serialize)]
-pub struct SelectedCharacter;
-#[derive(Debug, Clone, Serialize)]
-pub struct DrawCard;
-#[derive(Debug, Clone, Serialize)]
-pub struct BuyAsset;
-#[derive(Debug, Clone, Serialize)]
-pub struct IssueLiability;
-
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(tag = "action", content = "data")]
 pub enum ReceiveData {
